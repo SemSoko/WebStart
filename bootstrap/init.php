6c4bdf76076ab20abc2ac24394784e5b81fb0cce
SemSoko <info@semensokolski.de>
2025-05-19 14:29:34 +0200

    Aktuell: Im Prozess der Projektstrukturierung. init.php angepasst, zentralisiertes einbinden aller Module/Pakete.

--- bootstrap/init.php
+++ bootstrap/init.php
@@ -1,6 +1,15 @@
 <?php
-	require_once __DIR__ . '/../vendor/autoload.php';
-	
+	define('BASE_PATH', dirname(__DIR__));
+
+	//	Framework Module
+	require_once BASE_PATH . '/vendor/autoload.php';
+	require_once BASE_PATH . '/db.php';
+	require_once BASE_PATH . '/funktionen.php';
+	require_once BASE_PATH . '/JwtHandler.php';
+	require_once BASE_PATH . '/auth.php';
+	require_once BASE_PATH . '/todo.php';
+
+	//	Umgebungsvariablen aus .env-Datei
 	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 	$dotenv->load();
 ?>
