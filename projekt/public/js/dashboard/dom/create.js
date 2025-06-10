d866173b8ca302d76dbd7ca2ad3d9cd763ed5ce7
SemSoko <info@semensokolski.de>
2025-06-10 11:25:28 +0200

    docs(frontend/dashboard/dom): document createTodoListItem and createEmptyTodoMessage

--- projekt/public/js/dashboard/dom/create.js
+++ projekt/public/js/dashboard/dom/create.js
@@ -1,6 +1,18 @@
 import {createTextElement} from "./../../shared/dom/elements.js"
 
-// Erzeugt die leere Todo-Nachricht (z.B. wenn keine Todos vorhanden sind)
+/**
+ * @typedef {Object} TodoItem
+ * @property {number} todo_id - Die eindeutige ID des Todos.
+ * @property {string} todo_title - Der Titel des Todos.
+ * @property {number} todo_status - 1 fuer erledigt, 0 fuer offen.
+ * @property {string} todo_iat - Zeitstempel der Erstellung (ISO-Format).
+ */
+
+/**
+ * Erzeugt Todo-Nachricht fuer leere Todo-Liste
+ *
+ * @returns {HTMLLiElement} Li-Element mit eingegebenem Textinhalt
+ */
 export function createEmptyTodoMessage(){
 	const li = createTextElement("li", "Nothing to do - Relax");
 	li.dataset.empty = "true";
@@ -8,60 +20,90 @@ export function createEmptyTodoMessage(){
 	return li;
 }
 
-/*
- Erzeugt ein vollstaendiges Listenelement fuer ein Todo
- {onToggle, onDelete}: Eventhandler, die beim Statuswechsel oder
- Loeschen aufgerufen werden
-*/
-
+/**
+ * Erzeugt einen vollstaendigen Eintrag fuer die Todo-Liste
+ * 
+ * @param {TodoItem} todo - Ein einzelnes Todo-Objekt aus der API.
+ * @param {{
+ *    onToggle: function(number): void,
+ *    onDelete: function(number): void
+ *  }} callbacks
+ * @returns {HTMLLiElement} Der vollstaendige Eintrag fuer die Todo-Liste
+ */
 export function createTodoListItem(todo, {onToggle, onDelete}){
 	// Container <li> und inneres <div>
+	/**
+	 * Erstellen des Listeneintrags, welcher angehaengt wird
+	 * Erstellen eines Containers, in welchen die Todo-Elemente eingefuegt werden
+	 */
 	const li = document.createElement("li");
 	const textDiv = document.createElement("div");
 	
+	/**
+	 * Jedem Todo wird eine ID hinzugefuegt
+	 */
 	li.dataset.id = todo?.todo_id;
 	
-	// Todo-Titel <h3>
+	/**
+	 * Erstellen des Titels fuer ein Todo
+	 */
 	const todoTitle = createTextElement("h3", (todo?.todo_title ?? "No title"));
 	
-	// Checkbox zur Statusanzeige/-aenderung
+	/**
+	 * Statusanzeige eines Todos
+	 */
 	const todoCheckbox = document.createElement("input");
 	todoCheckbox.type = "checkbox";
 	todoCheckbox.dataset.id = todo?.todo_id;
 	todoCheckbox.checked = todo?.todo_status == 1;
-	
-	// Label fuer die Checkbox mit Status-Text
+
+	/**
+	 * Klickbares Label fuer die Statusanzeige eines Todos
+	 */
 	const checkboxLabel = document.createElement("label");
 	checkboxLabel.appendChild(todoCheckbox);
 	checkboxLabel.appendChild(
 		document.createTextNode(todo?.todo_status == 1 ? " Done" : " In progress")
 	);
 	
-	// Zeitstempel <h4>
+	/**
+	 * Erstellungsdatum des Todos
+	 */
 	const todoIat = createTextElement("h4", (todo?.todo_iat ?? "No date"));
 	
-	// Delete-Button
+	/**
+	 * Button zum Loeschen eines Todos
+	 */
 	const deleteButton = document.createElement("button");
 	deleteButton.textContent = "Delete";
 	deleteButton.dataset.id = todo?.todo_id;
 	
-	// Alles ins Text-Div einhaengen
+	/**
+	 * Alle Komponenten eines Todos im Container buendeln
+	 */
 	textDiv.appendChild(todoTitle);
 	textDiv.appendChild(checkboxLabel);
 	textDiv.appendChild(todoIat);
 	textDiv.appendChild(deleteButton);
 	
-	// Div in das Listen-Element einhaengen
+	/**
+	 * Container mit gebuendelten Todo-Komponenten in die Todo-Liste einhaengen
+	 */
 	li.appendChild(textDiv);
 	
-	// Eventhandler binden, nur wenn todo_id existiert
+	/**
+	 * Eventhandler fuer Todo-Statusanzeige
+	 */
 	if(todo?.todo_id){
-		// Beim Statuswechsel die uebergebene Callback-Funktion onToggle ausfuehren
+		/**
+		 * Todo-Statuswechsel-Callback - Done oder In progress
+		 */
 		todoCheckbox.addEventListener("change", () => onToggle(todo.todo_id));
-		// Beim Klick auf "Delete" die uebergebene Callback-Funktion onDelete ausfuehren
+		/**
+		 * Todos-Loeschen-Callback
+		 */
 		deleteButton.addEventListener("click", () => onDelete(todo.todo_id));
 	}
 	
-	// Rueckgabe des vollstaendigen Listenelements <li>
 	return li;
 }
