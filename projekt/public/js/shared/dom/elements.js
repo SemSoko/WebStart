cb46abb898ad5ca67b889149595efabc3fef898e
SemSoko <info@semensokolski.de>
2025-06-10 09:11:07 +0200

    docs(frontend/shared/elements): add JSDoc comments for dashboard DOM elements

--- projekt/public/js/shared/dom/elements.js
+++ projekt/public/js/shared/dom/elements.js
@@ -1,4 +1,9 @@
-//	Erzeugt ein HTML-Element mit Textinhalt
+/**
+ * HTML-Element mit Textinhalt erzeugen
+ * @param {string} tagName - Zu erstellendes HTML-Tag.
+ * @param {string} text - Inhalt des HTML-Elements.
+ * @returns {HTMLElement} Das erzeugte DOM-Element.
+ */
 export function createTextElement(tagName, text){
 	const el = document.createElement(tagName);
 	el.textContent = text;
