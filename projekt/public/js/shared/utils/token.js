8f9f0aa415e07ffdef1a6c9c7747f541225a8e4e
SemSoko <info@semensokolski.de>
2025-06-10 10:14:49 +0200

    docs(frontend/shared/utils): add JSDoc for getToken

--- projekt/public/js/shared/utils/token.js
+++ projekt/public/js/shared/utils/token.js
@@ -1,3 +1,8 @@
+/**
+ * Ermittelt den lokal gesetzten JWT-Token
+ *
+ * @returns {string|null} JWT-Token der aktuellen Sitzung oder null, wenn keiner gespeichert ist.
+ */
 export function getToken(){
 	return localStorage.getItem("jwt_token");
 }
