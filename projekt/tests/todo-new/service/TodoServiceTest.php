e172b81b905089112d321cfed24ab85cb05aedf5
SemSoko <info@semensokolski.de>
2025-07-01 16:29:32 +0200

    test(backend/todo-new/service/addTodo): add unit tests for addTodo in TodoService

---
+++ projekt/tests/todo-new/service/TodoServiceTest.php
@@ -0,0 +1,83 @@
+<?php
+	require_once __DIR__ . '/../../base/DatabaseTestCasePreparation.php';
+	require_once __DIR__ . '/../../../src/todo-new/repository/TodoRepository.php';
+	require_once __DIR__ . '/../../../src/todo-new/service/TodoService.php';
+	
+	class TodoServiceTest extends DatabaseTestCase{
+		
+		protected function createSchema(): void{
+			$this->pdo->exec("
+				create table users(
+					id integer primary key autoincrement,
+					email varchar(255) unique not null,
+					password varchar(255) not null,
+					created_at timestamp default current_timestamp
+				);
+			");
+			
+			$this->pdo->exec("
+				CREATE TABLE todos(
+					id INTEGER PRIMARY KEY AUTOINCREMENT,
+					user_id INT NOT NULL,
+					title VARCHAR(255) NOT NULL,
+					is_done BOOLEAN DEFAULT FALSE,
+					created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+					FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+				);
+			");
+		}
+		
+		protected function seedTestData(): void{
+			$stmt = $this->pdo->prepare("insert into users (email, password) values (?, ?)");
+			$stmt->execute(['test@test.de', password_hash('EinPasswort123-', PASSWORD_DEFAULT)]);
+		}
+
+		public function testAddTodoReturnsSuccessOnValidInput(): void{
+			$repository = new TodoRepository($this->pdo);
+			$service = new TodoService($repository);
+			
+			$userId = 1;
+			$title = 'Einkaufen';
+			
+			$result = $service->addTodo($title, $userId);
+			
+			$expectedResult = ['success' => true];
+			$this->assertSame($result, $expectedResult);
+		}
+		
+		public function testAddTodoForwardsRepositoryError(): void{
+			$mockRepo = $this->createMock(TodoRepository::class);
+			$mockRepo->method('insertTodo')->willReturn([
+				'success' => false,
+				'error' => 'INSERT fehlgeschlagen',
+				'debug' => [
+					'errorInfo' => 'MariaDB-Info'
+				],
+				'source' => 'todo-new/repository'
+			]);
+			
+			$service = new TodoService($mockRepo);
+			
+			$userId = 1222;
+			$title = 'Einkaufen';
+			$result = $service->addTodo($title, $userId);
+			
+			$this->assertFalse($result['success']);
+			$this->assertSame('todo-new/repository', $result['source']);
+		}
+		
+		public function testAddTodoReturnsServiceErrorOnException(): void{
+			$mockRepo = $this->createMock(TodoRepository::class);
+			$mockRepo->method('insertTodo')->willThrowException(new PDOException('Integrity constraint violation: FOREIGN KEY constraint failed'));
+			
+			$service = new TodoService($mockRepo);
+			
+			$userId = 1;
+			$title = 'Einkaufen';
+			$result = $service->addTodo($title, $userId);
+			
+			$this->assertFalse($result['success']);
+			$this->assertSame('service', $result['source']);
+			$this->assertStringContainsString('FOREIGN KEY constraint failed', $result['error']);
+		}
+	}
