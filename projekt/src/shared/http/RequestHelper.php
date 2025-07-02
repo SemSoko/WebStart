900eff7fbfac438eafdf2897cabb39b11ef5640e
SemSoko <info@semensokolski.de>
2025-07-02 16:52:06 +0200

    chore(backend/shared/http): add dummy for structure

---
+++ projekt/src/shared/http/RequestHelper.php
@@ -0,0 +1,7 @@
+<?php
+	/**
+	 * Zugriff auf $_SERVER ist Infrastruktur
+	 * Diese Klasse hat eine Aufgabe (SRP): HTTP-Header lesen
+	 * Sie ist modulunabhängig und projektweit wiederverwendbar
+	 * „Helper“ ist ein gängiger Begriff für solche Hilfsdienste
+	 */
