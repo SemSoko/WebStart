eb6d15139d57420cf984e40ba0a28ee69b73f9fe
SemSoko <info@semensokolski.de>
2025-07-07 20:34:38 +0200

    test(backend/shared/response): fix output buffering in success() test

--- projekt/tests/shared/response/JsonResponseHandlerTest.php
+++ projekt/tests/shared/response/JsonResponseHandlerTest.php
@@ -1,11 +1,39 @@
 <?php
 	require_once __DIR__ . '/../../base/DatabaseTestCasePreparation.php';
 	require_once __DIR__ . '/../../../src/shared/response/JsonResponseHandler.php';
+	require_once __DIR__ . '/TestableJsonResponseHandler.php';
 	
 	use Shared\Response\JsonResponseHandler;
 	
 	class JsonResponseHandlerTest extends DatabaseTestCase{
 		public function setUp(): void{
 			parent::setUp();
+		}
+		
+		public function testSuccessOutputsExpectedJsonAndExits(): void{
+			$handler = new class extends TestableJsonResponseHandler{				
+				public function success(array $data = [], int $statusCode = 200): void{
+					http_response_code($statusCode);
+					echo(json_encode([
+						'success' => true,
+						'data' => $data
+					]));
+					$this->exit();
+				}
+			};
+			
+			// Buffer starten
+			ob_start();
+			$data = ['foo' => 'bar'];
+			$handler->success($data, 201);
+			// Buffer auslesen und beenden
+			$output = ob_get_clean();
+			
+			$json = json_decode($output, true);
+			
+			$this->assertSame(201, http_response_code());
+			$this->assertTrue($json['success']);
+			$this->assertSame($data, $json['data']);
+			$this->assertTrue($handler->hasExited());
 		}
 	}
