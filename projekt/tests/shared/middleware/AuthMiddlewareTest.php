a4a1fcf9f0b1da09e853bbe2b056731d03e74943
SemSoko <info@semensokolski.de>
2025-07-09 13:06:15 +0200

    test(backend/shared/middleware): add test for missing token

---
+++ projekt/tests/shared/middleware/AuthMiddlewareTest.php
@@ -0,0 +1,47 @@
+<?php
+	require_once __DIR__ . '/../../base/DatabaseTestCasePreparation.php';
+	require_once __DIR__ . '/../../../src/shared/middleware/AuthMiddleware.php';
+	require_once __DIR__ . '/../../../src/shared/auth/AuthServiceInterface.php';
+	require_once __DIR__ . '/../../../src/shared/http/RequestTokenReaderInterface.php';
+	require_once __DIR__ . '/../response/TestableJsonResponseHandler.php';
+	
+	use Shared\Middleware\AuthMiddleware;
+	use PHPUnit\Framework\MockObject\MockObject;
+	use Shared\Auth\AuthServiceInterface;
+	use Shared\Http\RequestTokenReaderInterface;
+	use Shared\Response\ResponseHandlerInterface;
+	use Tests\Shared\Response\TestableJsonResponseHandler;
+	
+	class AuthMiddlewareTest extends DatabaseTestCase{
+		private MockObject $authService;
+		private MockObject $tokenReader;
+		private MockObject $response;
+		
+		public function setUp(): void{
+			parent::setUp();
+			
+			$this->authService = $this->createMock(AuthServiceInterface::class);
+			$this->tokenReader = $this->createMock(RequestTokenReaderInterface::class);
+			$this->response = $this->createMock(ResponseHandlerInterface::class);
+		}
+		
+		public function testReturnsDebugWhenNoTokenProvided(): void{
+			$this->tokenReader
+				->method('getBearerToken')
+				->willReturn(null);
+				
+			$response = new TestableJsonResponseHandler();
+			
+			$middleware = new AuthMiddleware(
+				$this->authService,
+				$response,
+				$this->tokenReader
+			);
+			
+			$this->expectException(\RuntimeException::class);
+			$this->expectExceptionMessage('Mocked debug: Kein Token uebergeben (401)');
+			
+			// Test: Methode aufrufen -> erwartet, dass debug() aufgerufen wird
+			$middleware->handle();
+		}
+	}
