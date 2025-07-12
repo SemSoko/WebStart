8635e83195550b6449edc86c3098e126040a91cc
SemSoko <info@semensokolski.de>
2025-07-12 07:35:06 +0200

    test(backend/todo-new/service): remove legacy DB setup from unit test

--- projekt/tests/todo-new/service/TodoServiceTest.php
+++ projekt/tests/todo-new/service/TodoServiceTest.php
@@ -1,44 +1,16 @@
 <?php
 	require_once __DIR__ . '/../../base/UnitTestCase.php';
-	require_once __DIR__ . '/../../../src/todo-new/repository/TodoRepository.php';
 	require_once __DIR__ . '/../../../src/todo-new/service/TodoService.php';
+	require_once __DIR__ . '/../../../src/todo-new/repository/TodoRepository.php';
 	
 	use todoNew\Service\TodoService;
-	use todoNew\Repository\TodoRepository;
 	use Tests\Base\UnitTestCase;
+	use todoNew\Repository\TodoRepository;
 	
 	class TodoServiceTest extends UnitTestCase{
-		
-		protected function createSchema(): void{
-			$this->pdo->exec("
-				create table users(
-					id integer primary key autoincrement,
-					email varchar(255) unique not null,
-					password varchar(255) not null,
-					created_at timestamp default current_timestamp
-				);
-			");
-			
-			$this->pdo->exec("
-				CREATE TABLE todos(
-					id INTEGER PRIMARY KEY AUTOINCREMENT,
-					user_id INT NOT NULL,
-					title VARCHAR(255) NOT NULL,
-					is_done BOOLEAN DEFAULT FALSE,
-					created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-					FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-				);
-			");
-		}
-		
-		protected function seedTestData(): void{
-			$stmt = $this->pdo->prepare("insert into users (email, password) values (?, ?)");
-			$stmt->execute(['test@test.de', password_hash('EinPasswort123-', PASSWORD_DEFAULT)]);
-		}
-
 		public function testAddTodoReturnsSuccessOnValidInput(): void{
-			$repository = new TodoRepository($this->pdo);
-			$service = new TodoService($repository);
+			$repository = new TodoRepository($this->pdo); // muss gemockt werden
+			$service = new TodoService($repository); // mit mock an interface uebergeben werden
 			
 			$userId = 1;
 			$title = 'Einkaufen';
@@ -71,6 +43,7 @@
 		}
 		
 		public function testAddTodoReturnsServiceErrorOnException(): void{
+			// muss ebenfalls gemockt werden
 			$repository = new TodoRepository($this->pdo);
 			$service = new TodoService($repository);
 			
