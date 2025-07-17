618f0d8d4e8c5c2de01ae3fe186d4a04ca92e9da
SemSoko <info@semensokolski.de>
2025-07-17 10:51:44 +0200

    docs(backend/todo-new/service): add PHPdoc to TodoServiceInterface

--- projekt/src/todo-new/service/TodoServiceInterface.php
+++ projekt/src/todo-new/service/TodoServiceInterface.php
@@ -2,12 +2,23 @@
 	namespace TodoNew\Service;
 	
 	/**
-	 * Schnittstelle fuer den Todo-Service.
+	 * Schnittstelle fuer die Geschaeftslogik im todo-new-Modul.
 	 *
-	 * Definiert die oeffentlich zugaengliche Logik fuer das
-	 * Hinzufuegen von Todos, unabhaengig von der konkreten
-	 * Implementierung oder dem verwendenten Repository.
+	 * Definiert die oeffentlichen Methoden rund um das todo-new-Modul.
+	 * Die Implementierung kann beliebig sein.
+	 * Ziel: Trennung von Logik und Infrastruktur durch saubere Abstraktion.
+	 *
+	 * @package TodoNew\Service
 	 */
 	interface TodoServiceInterface{
+		/**
+		 * Fuegt ein neues Todo fuer einen Benutzer hinzu.
+		 *
+		 * @param string $title Titel des Todos (muss bereits validiert sein)
+		 * @param int $userId ID des angemeldeten Benutzers
+		 * @return array
+		 * Ergebnisstruktur (z.B.: ['success' => true, 'id' => 123])
+		 * oder Fehlerstruktur
+		 */
 		public function addTodo(string $title, int $userId): array;
 	}
