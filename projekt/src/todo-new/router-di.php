bc2dbe91f84e4c4f47b435500862d357f85cb12d
SemSoko <info@semensokolski.de>
2025-07-18 12:18:18 +0200

    refactor(backend/todo-new/router): delegate request handling to TodoRouter

--- projekt/src/todo-new/router-di.php
+++ projekt/src/todo-new/router-di.php
@@ -1,19 +1,26 @@
 <?php
-	require_once __DIR__ . '/../../bootstrap/init.php';
-
 	/*
-	 * DI-Container und notwendige Konfigurationen einbinden
+	 * Einstiegspunkt für das todo-new Modul
 	 */
-	require_once __DIR__ . '/../shared/service-container/Container.php';
-	require_once __DIR__ . '/../shared/service-container/ServiceIds.php';
-	require_once __DIR__ . '/TodoModule.php';
+
+	require_once __DIR__ . '/../../bootstrap/init.php';
+	require_once __DIR__ . '/TodoRouter.php';
 	
 	use Shared\ServiceContainer\Container;
-	use Shared\ServiceContainer\ServiceIds;
 	use TodoNew\TodoModule;
+	use TodoNew\TodoRouter;
 	
 	/*
-	 * Erstellt eine neue Instanz des DI-Containers
+	 * Ermittelt die HTTP-Methode der Anfrage (z.B.: GET, POST)
+	 */
+	$method = $_SERVER['REQUEST_METHOD'];
+	/*
+	 * URI
+	 */
+	$uri = $_SERVER['REQUEST_URI'];
+	
+	/*
+	 * Erstellt DI-Container
 	 */
 	$container = new Container();
 	
@@ -23,95 +30,7 @@
 	TodoModule::register($container);
 	
 	/*
-	 * Ermittelt die HTTP-Methode der Anfrage (z.B.: GET, POST)
+	 * Modul-Router ausfuehren.
 	 */
-	$method = $_SERVER['REQUEST_METHOD'];
-	
-	/*
-	 * Extrahiert den Pfad aus der vollstaendigen Request-URI
-	 */
-	$requestUri = rtrim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
-
-	/*
-	 * Routing-Konfiguration: Welche URI fuehrt zu welcher Controller-Methode?
-	 * Diese Zuordnung ist statisch. Dynamische Pfade koennen spaeter ergaenzt werden.
-	 */
-
-	/**
-	 * @todo
-	 * Dynamische Pfade (z. B. /api/todo-new/{id}) und Middleware (z. B. Auth)) ergaenzen
-	 */
-	$routes = [
-		'GET' => [
-			'/api/todo-new' => [TodoController::class, 'getAll'],
-			/*
-			 * Sonderroute fuer Status-Check
-			 */
-			'/api/todo.php/status' => '__status__'
-		],
-		'POST' => [
-			'/api/todo-new' => [TodoController::class, 'add'],
-		],
-		'PATCH' => [
-			'/api/todo-new' => [TodoController::class, 'toggleStatus'],
-		],
-		'DELETE' => [
-			'/api/todo-new' => [TodoController::class, 'delete'],
-		]
-	];
-	
-	/*
-	 * Versucht, eine passende Route basierend auf Methode und Pfad zu finden.
-	 */
-	$matchedRoute = $routes[$method][$requestUri] ?? null;
-	
-	/*
-	 * Falls keine passende Route existiert: Fehlermeldung mit Status 404.
-	 */
-	if(!$matchedRoute){
-		$response = $container->get(ServiceIds::RESPONSE);
-		$response->error('Route not found', 404);
-	}
-	
-	/*
-	 * Sonderfall: Status-Rueckmeldung, um die Erreichbarkeit zu pruefen.
-	 */
-	if($matchedRoute === '__status__'){
-		$response = $container->get(ServiceIds::RESPONSE);
-		$response->status('Todo-Modul erreichbar');
-		exit();
-	}
-
-	/*
-	 * Zerlegt die Route in Klasse und Methodenname
-	 */
-	[$controllerClass, $methodName] = $matchedRoute;
-	
-	/*
-	 * Entscheidet anhand der Controller-Klasse, welche
-	 * konkrete Instanz aus dem Container geladen werden soll.
-	 */
-	switch($controllerClass){
-		/*
-		 * Entscheidet, welcher Controller fuer die aktuelle Route
-		 * verwendet werden soll.
-		 */
-		case TodoController::class:
-			/*
-			 * Holt den TodoController aus dem DI-Container.
-			 */
-			$controller = $container->get(ServiceIds::TODO_CONTROLLER);
-			break;
-		
-		/*
-		 * Sicherheitsmechanismus fuer nicht registrierte Controller-Klassen.
-		 */
-		default:
-			$response = $container->get(ServiceIds::RESPONSE);
-			$response->error('Unbekannter Controller', 500);
-	}
-	
-	/*
-	 * Fuehrt die dem Pfad zugewiesene Methode des Controllers aus.
-	 */
-	$controller->$methodName();
+	$router = new TodoRouter($container);
+	$router->dispatch($method, $uri);
