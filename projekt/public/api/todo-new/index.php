8c9524acabde7430a4560f1aff07cf5b4cfb3916
SemSoko <info@semensokolski.de>
2025-07-18 16:46:22 +0200

    refactor(frontend/api/todo-new): replace legacy router-di with new entrypoint

--- projekt/public/api/todo-new/index.php
+++ projekt/public/api/todo-new/index.php
@@ -1,2 +1,3 @@
 <?php
-	require_once __DIR__ . '/../../../src/todo-new/router-di.php';
+	// require_once __DIR__ . '/../../../src/todo-new/router-di.php';
+	require_once __DIR__ . '/../../../src/entrypoints/todo-new.php';
